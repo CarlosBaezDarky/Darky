5bf38942c2ef285d2d96431a34c33322f242a431
Carlos E. Baez <playoung11@gmail.com>
2021-03-18 15:38:52 -0400

    prueba a ver si se crear otro acrchivo programs

---
+++ probandoApp/Program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace probandoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int a;
+            try {
+
+                int i;
+
+                i = int.Parse(Console.ReadLine());
+            }
+            catch (System.Exception e) when (e.GetType() != typeof(FormatException)) {} 
+             
+        }
+    }
+}
